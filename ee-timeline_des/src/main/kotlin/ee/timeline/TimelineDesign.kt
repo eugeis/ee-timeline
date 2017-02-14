e104eb82f18c584300e7c324f5a4cd703359e758
Eugen Eisler <eoeisler@gmail.com>
2017-02-14 11:23:17 +0100

    Introduce DesignKotlinContextFactory

--- ee-timeline_des/src/main/kotlin/ee/timeline/TimelineDesign.kt
+++ ee-timeline_des/src/main/kotlin/ee/timeline/TimelineDesign.kt
@@ -3,53 +3,46 @@ package ee.timeline
 import ee.design.*
 import ee.lang.*
 
-object TimelineDesign : Model({ namespace("ee") }) {
-    object Timeline : Comp() {
-        object shared : Module() {
+object Timeline : Comp({ namespace("ee.timeline") }) {
+    object shared : Module() {
 
-            object TimeEvents : Entity() {
-                val title = prop(TimeEvent)
-                val events = prop(n.List.GT(TimeEvent))
-                val eras = prop(n.List.GT(Era))
-            }
-
-            object Era : Entity() {
-                val start = prop(n.Date)
-                val end = prop(n.Date)
-                val text = prop(Text)
-            }
-
-            object Text : Basic() {
-                val title = prop()
-                val text = prop()
-            }
-
-            object Media : Basic() {
-                val url = prop()
-                val caption = prop()
-                val credit = prop()
-                val thumbnail = prop()
-            }
-
-            object TimeEvent : Entity({ superUnit(Era) }) {
-                val group = prop()
-                val media = prop(Media)
-            }
+        object TimeEvents : Entity() {
+            val title = prop(TimeEvent)
+            val events = prop(n.List.GT(TimeEvent))
+            val eras = prop(n.List.GT(Era))
         }
 
-        object person : Module() {
-            object Author : Entity() {
-                val firstName = prop()
-                val lastName = prop()
-                val birthsDate = prop(n.Date)
-                val deathsDate = prop(n.Date)
-                val quotes = prop(n.List.GT())
-            }
+        object Era : Entity() {
+            val start = prop(n.Date)
+            val end = prop(n.Date)
+            val text = prop(Text)
+        }
+
+        object Text : Basic() {
+            val title = prop()
+            val text = prop()
+        }
+
+        object Media : Basic() {
+            val url = prop()
+            val caption = prop()
+            val credit = prop()
+            val thumbnail = prop()
+        }
+
+        object TimeEvent : Entity({ superUnit(Era) }) {
+            val group = prop()
+            val media = prop(Media)
         }
     }
-}
 
-fun model(): Model {
-    n.initObjectTree()
-    return TimelineDesign.initObjectTree()
+    object person : Module() {
+        object Author : Entity() {
+            val firstName = prop()
+            val lastName = prop()
+            val birthsDate = prop(n.Date)
+            val deathsDate = prop(n.Date)
+            val quotes = prop(n.List.GT())
+        }
+    }
 }
--- ee-timeline_des/src/main/kotlin/ee/timeline/TimelineGenerator.kt
+++ ee-timeline_des/src/main/kotlin/ee/timeline/TimelineGenerator.kt
@@ -1,8 +1,9 @@
 package ee.timeline
 import ee.design.gen.DesignKotlinGenerator
+import ee.lang.integ.dPath
 import ee.lang.integ.eePath
 
 fun main(args: Array<String>) {
-    val generator = DesignKotlinGenerator(model())
-    generator.generate(eePath)
+    val generator = DesignKotlinGenerator(Timeline)
+    generator.generate(dPath)
 }

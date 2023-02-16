659c7adc1bc14b5deade535a9d4f41f7d3127645
Eugen Eisler <eoeisler@gmail.com>
2023-02-16 11:55:14 +0100

    Working state

--- ee-timeline_des/src/main/kotlin/ee/timeline/TimelineGenerator.kt
+++
@@ -1,9 +0,0 @@
-package ee.timeline
-
-import ee.design.gen.kt.DesignKotlinGenerator
-import ee.lang.integ.dPath
-
-fun main(args: Array<String>) {
-    val generator = DesignKotlinGenerator(Timeline)
-    generator.generate(dPath)
-}
